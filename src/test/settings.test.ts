d6d24457457c1f7fd08415f562537d5af1f8de76
Ahmed T. Ali <ah.tajelsir@gmail.com>
2018-04-21 22:56:03 +0300

    tests: ensure configurations are being setup/disabled correctly

---
+++ src/test/settings.test.ts
@@ -0,0 +1,44 @@
+// Packages
+import * as assert from "assert";
+import { workspace } from "vscode";
+
+// Ours
+import { setup, reset } from "../settings";
+
+suite("Settings", function() {
+	test("Setup configurations", async () => {
+		// Run!
+		await setup();
+
+		// Workbench
+		const wb = workspace.getConfiguration("workbench");
+		assert.equal(wb.get("editor.showTabs"), false);
+		assert.equal(wb.get("activityBar.visible"), false);
+
+		// Window
+		const w = workspace.getConfiguration("window");
+		assert.equal(w.get("menuBarVisibility"), "toggle");
+
+		// Explorer
+		const e = workspace.getConfiguration("explorer");
+		assert.equal(e.get("openEditors.visible"), false);
+	});
+
+	test("Reset configurations", async () => {
+		// Run!
+		await reset();
+
+		// Workbench
+		const wb = workspace.getConfiguration("workbench");
+		assert.equal(wb.get("editor.showTabs"), true);
+		assert.equal(wb.get("activityBar.visible"), true);
+
+		// Window
+		const w = workspace.getConfiguration("window");
+		assert.equal(w.get("menuBarVisibility"), "default");
+
+		// Explorer
+		const e = workspace.getConfiguration("explorer");
+		assert.equal(e.get("openEditors.visible"), 9);
+	});
+});
